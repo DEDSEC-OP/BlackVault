38c46ae7f2bdc2181f209f89dec123507c0cca4c
DEDSEC-OP <jacebandz23@gmail.com>
2025-04-08 04:48:12 -0500

    Create Card_tools.sh

---
+++ Card_tools.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Card Tools Placeholder
+
+echo -e "\n[+] Launching Card Tools Suite..."
+echo "[!] This feature is under development."
+# Future: BIN lookup, card format checker, Luhn algorithm validator, etc.
