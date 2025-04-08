e150d4bccd82e57225fa821ac33802dab20148f3
DEDSEC-OP <jacebandz23@gmail.com>
2025-04-08 04:49:24 -0500

    Create Carhacking.sh

---
+++ Carhacking.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Car Hacking Utilities (Concept)
+
+echo -e "\n[+] Starting Car Hacking Toolkit..."
+echo "[!] Feature ideas:"
+echo "- CAN Bus sniffing"
+echo "- Key fob replay attacks"
+echo "- Diagnostic code scans"
+# Future modules could interface with USB OBD-II readers etc.
